1ac9e899d4dab993641511f1d81a422ffca5fc01
CoteleaTudor <104974457+CoteleaTudor@users.noreply.github.com>
2022-11-21 21:44:13 +0200

    ex_11_2d-for-pattern_finisat

---
+++ ex_11_2d-for-pattern_finisat.py
@@ -0,0 +1,46 @@
+# * * * * * * * * * * y1
+# * * * * * * * * * * 
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * * y10
+rx  = 5
+ry  = 5
+
+print()
+
+for y in range (1, 11):
+    for x in range (1, 11):
+        if x == rx and y == ry:
+            print("R", end=" ")
+        # diagonals
+        elif x == rx - 1 and y == ry - 1:
+            print("+", end=" ")
+        elif x == rx + 1 and y == ry + 1:
+            print("+", end=" ")
+        elif x == rx + 1 and y == ry - 1:
+            print("+", end=" ")
+        elif x == rx - 1 and y == ry + 1:
+            print("+", end=" ")           
+        # up and down
+        elif x == rx and y == ry - 1:
+            print("+", end=" ")
+        elif x == rx and y == ry + 1:
+            print("+", end=" ")
+        # left and right    
+        elif x == rx - 1 and y == ry:
+            print("+", end=" ")
+        elif x == rx + 1 and y == ry:
+            print("+", end=" ")                      
+        
+        else:
+            print(".", end=" ")    
+    print()
+
+print()
+
+#HW1*: Diagonal directions

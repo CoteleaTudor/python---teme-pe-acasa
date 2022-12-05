f5e490502baffcc85fbfba8fcaad6ac87deb81a4
CoteleaTudor <104974457+CoteleaTudor@users.noreply.github.com>
2022-12-05 20:41:29 +0200

    lista simpla - for
    afisarea listei prin for de sus in jos

---
+++ list_2.py
@@ -0,0 +1,30 @@
+# bideminsional / 2 levels
+destinations = [
+    [           
+        "Paris",        # 0
+        "Rome",         # 1
+        "London",       # 2
+    ],
+    [           # index 1
+        "NY",
+        "Totonto",
+        "Florida",
+    ]
+]
+#destinations[0] = "UE"
+#destinations[1] = "SUA"
+for x in destinations:
+    for y in x:
+        print(y)
+    
+        
+#destinations[0][2] = 'Pisa'
+#print( destinations[0][2])
+
+# HW1: using for inside for print al the destinations
+# try
+# UE:
+# - Paris
+# SUA:
+# - NY
+# etc .....
